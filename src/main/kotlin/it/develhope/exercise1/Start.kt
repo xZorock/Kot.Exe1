8ef0eaea80af4b6ee7556da446d7d1be1fb9c909
Jason Di Bartolo <jasondibartolo98@gmail.com>
2023-01-02 17:59:38 +0100

    Initial Commit

---
+++ src/main/kotlin/it/develhope/exercise1/Start.kt
@@ -0,0 +1,10 @@
+package it.develhope.exercise1
+
+fun main(){
+    val n1 = 123
+    val s1 = "Hello"
+    val b1 = true
+
+    println("First variable is : $n1 \nSecond variable is : $s1 \nThirst variable is $b1")
+
+}
